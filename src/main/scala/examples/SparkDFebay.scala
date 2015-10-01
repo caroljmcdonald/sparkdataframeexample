b02ce9354afb1d055ee312527063fe55dd71107a
Carol McDonald <caroljmcdonald@gmail.com>
2015-10-01 19:22:45 -0400

    remove some files

--- src/main/scala/examples/SparkDFebay.scala
+++ src/main/scala/examples/SparkDFebay.scala
@@ -17,7 +17,7 @@
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
+
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql._
 
